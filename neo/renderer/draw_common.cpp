ac46bb6a3121a9e38c95ae300e9d7e9ac68433da
Pat Raynor <raynorpat@gmail.com>
2015-03-08 14:01:32 -0400

    Cleanup unused stencil test in RB_STD_FogAllLights

--- neo/renderer/draw_common.cpp
+++ neo/renderer/draw_common.cpp
@@ -1374,9 +1374,7 @@ RB_STD_FogAllLights
 void RB_STD_FogAllLights( void ) {
 	viewLight_t	*vLight;
 
-	if ( r_skipFogLights.GetBool() || r_showOverDraw.GetInteger() != 0 
-		 || backEnd.viewDef->isXraySubview /* dont fog in xray mode*/
-		 ) {
+	if ( r_skipFogLights.GetBool() || r_showOverDraw.GetInteger() != 0 || backEnd.viewDef->isXraySubview ) {
 		return;
 	}
 
@@ -1391,37 +1389,11 @@ void RB_STD_FogAllLights( void ) {
 			continue;
 		}
 
-#if 0 // _D3XP disabled that
-		if ( r_ignore.GetInteger() ) {
-			// we use the stencil buffer to guarantee that no pixels will be
-			// double fogged, which happens in some areas that are thousands of
-			// units from the origin
-			backEnd.currentScissor = vLight->scissorRect;
-			if ( r_useScissor.GetBool() ) {
-				qglScissor( backEnd.viewDef->viewport.x1 + backEnd.currentScissor.x1, 
-					backEnd.viewDef->viewport.y1 + backEnd.currentScissor.y1,
-					backEnd.currentScissor.x2 + 1 - backEnd.currentScissor.x1,
-					backEnd.currentScissor.y2 + 1 - backEnd.currentScissor.y1 );
-			}
-			qglClear( GL_STENCIL_BUFFER_BIT );
-
-			qglEnable( GL_STENCIL_TEST );
-
-			// only pass on the cleared stencil values
-			qglStencilFunc( GL_EQUAL, 128, 255 );
-
-			// when we pass the stencil test and depth test and are going to draw,
-			// increment the stencil buffer so we don't ever draw on that pixel again
-			qglStencilOp( GL_KEEP, GL_KEEP, GL_INCR );
-		}
-#endif
-
 		if ( vLight->lightShader->IsFogLight() ) {
 			RB_FogPass( vLight->globalInteractions, vLight->localInteractions );
 		} else if ( vLight->lightShader->IsBlendLight() ) {
 			RB_BlendLight( vLight->globalInteractions, vLight->localInteractions );
 		}
-		qglDisable( GL_STENCIL_TEST );
 	}
 
 	qglEnable( GL_STENCIL_TEST );
